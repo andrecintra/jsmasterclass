54e39684445138777ad9409e792ca40db70f0f69
Andre Cintra <andre.cintra@accurate.com.br>
2021-09-09 16:31:50 -0300

    Exercicio 6 e 7

---
+++ Exercicio 6 - Array/exercicio-6.js
@@ -0,0 +1,125 @@
+
+const filterSelectedElements = function(selectedElements, selectedColumns) {
+
+    return selectedElements.map((row) => {
+        let returnObject = {}
+
+        selectedColumns.forEach(key => {
+            returnObject[key] = row[key]
+        });
+
+        return returnObject
+    })
+
+}
+
+const createTable = function(command) {
+
+    const tableName = command.match(/table\s(\w+)\s/)[1]
+    const columns = command.match(/\((.+)\)/)[1].trim().split(', ')
+
+    this.tables = {
+        [tableName]: {
+            columns: {},
+            data: []
+        },
+    }
+
+    for (const column of columns) {
+        const splitArray = column.split(" ")
+        const name = splitArray[0]
+        const value = splitArray[1]
+        Object.assign(this.tables[tableName].columns, {[name]: value})
+    }
+
+}
+
+const DatabaseError = function (statement, message) {
+    this.statement = statement
+    this.message = message
+}
+
+const execute = function(command) {
+
+    if (command.startsWith("create table")) {
+        return this.createTable(command)
+    } else if(command.startsWith("insert into")) {
+        return this.insert(command)
+    } else if(command.startsWith("select")) {
+        return this.select(command)
+    }
+
+    throw new DatabaseError(command, `Syntax error: '${command}' `)
+}
+
+const insert = function(command) {
+
+    const tableName = command.match(/into\s(\w+)\s/)[1]
+    const columns = command.match(/\((.+?)\)/)[1].trim().split(', ')
+    const values = command.match(/\(.+?\).*?(\(.+?\))/)[1].trim().substring(1).slice(0, -1).split(', ')
+
+    let row = {}
+    for (let i = 0; i < columns.length; i++) {
+        row[columns[i]] = values[i]
+    }
+
+    this.tables[tableName].data.push(row)
+}
+
+const select = function(command) {
+
+    // const regexp = /select (.+) from ([a-z]+)(?: where (.+))?/;
+
+    let tableName
+    let columnsWhere
+    let valuesWhere
+    if (command.includes("where")){
+        tableName = command.match(/from\s(\w+)\s/)[1].trim()
+        columnsWhere = command.match(/where\s(.+)\s=/)[1].trim()
+        valuesWhere = command.match(/where\s.+\s=\s(.+)/)[1].trim()
+    } else {
+        tableName = command.match(/from\s(\w+)/)[1].trim()
+    }
+
+    const selectedColumns = command.match(/select\s(.+)\sfrom/)[1].trim().split(', ')
+
+    if (columnsWhere){
+
+        const filtered = this.tables[tableName].data.filter((row) => {
+            return row[columnsWhere] === valuesWhere
+        })
+
+        return filterSelectedElements(filtered, selectedColumns)
+    } 
+
+    return filterSelectedElements(this.tables[tableName].data, selectedColumns)
+    
+}
+
+const database = {
+    "tables": {},
+    createTable,
+    execute, 
+    insert,
+    select
+}
+
+try {
+
+    database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
+    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
+    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
+    const selectResult1 = database.execute("select name, age from author");
+    const selectResult2 = database.execute("select name, age from author where id = 1");
+
+    console.log("result1: %j", selectResult1)
+    console.log("result1: %j", selectResult2)
+    console.log(JSON.stringify(database, null, " "))
+    
+} catch (error) {
+    
+    console.error(error.message)
+}
+
+
